4a9efd18cd0a96936723049761368cb1eb2ace18
smtsuchi <shoha1999@gmail.com>
2023-04-11 21:53:25 -0400

    day 2

---
+++ app/models.py
@@ -0,0 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(45), nullable = False, unique = True)
+    email = db.Column(db.String(100), nullable = False, unique = True)
+    password = db.Column(db.String, nullable = False)
+    date_created = db.Column(db.DateTime, nullable = False, default=datetime.utcnow())
+    post = db.relationship('Post', backref = 'author', lazy = True)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def saveToDB(self):
+        db.session.add(self)
+        db.session.commit()
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable = False)
+    img_url = db.Column(db.String, nullable = False)
+    caption = db.Column(db.String(500))
+    date_created = db.Column(db.DateTime, nullable = False, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+    def __init__(self, title, img_url, caption, user_id):
+        self.title = title
+        self.img_url = img_url
+        self.caption = caption
+        self.user_id = user_id
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import redirect, render_template, request, url_for
 from app import app
+from .forms import SignUpForm
+from .models import Post, User
 
 @app.route('/')
 def homePage():
@@ -11,9 +13,29 @@ def homePage():
 
     return render_template('index.html', pop=people, more=more)
 
-@app.route('/signup', methods=["GET"])
+@app.route('/signup', methods=["GET", "POST"])
 def signupPage():
-    return render_template('signup.html')
+    form = SignUpForm()
+    
+    if request.method == 'POST':
+        if form.validate():
+            username = form.username.data
+            email = form.email.data
+            password = form.password.data
+
+            # add user to database
+            # if User exists:
+            #     print(error msg)
+            # else:
+            # user = User(username, email, password)
+        
+            # user.saveToDB()
+            account = {
+                'username': username,
+                'email': email
+            }
+        return render_template('signup.html', form = form, account=account)
+    return render_template('signup.html', form = form)
 
 @app.route('/login')
 def loginPage():
