75e97abdb3ab7d70d64a7e0b40d6933e1c4baa0a
smtsuchi <shoha1999@gmail.com>
2023-05-04 21:54:48 -0400

    react thursday

--- app/api/ig_routes.py
+++ app/api/ig_routes.py
@@ -1,5 +1,7 @@
 from . import api
 from ..models import Post
+from ..apiauthhelper import token_auth
+from flask import request
 
 
 @api.get('/posts')
@@ -25,3 +27,61 @@ def getPostAPI( post_id):
             'status': 'not ok',
             'message': 'The post you are looking for does not exist'
         }, 404
+
+@api.post('/posts/create')
+@token_auth.login_required
+def createPostAPI():
+    data = request.json
+    
+    title = data['title']
+    img_url = data['img_url']
+    caption = data['caption']
+
+    post = Post(title, img_url, caption, token_auth.current_user().id)
+
+    post.saveToDB()
+
+            
+    return {
+        'status': 'ok',
+        'message': 'Succesfully created a post.',
+        'post': post.to_dict()
+    }, 201
+
+@api.post('/posts/update/<int:post_id>')
+@token_auth.login_required
+def updatePostAPI(post_id):
+    post = Post.query.get(post_id)
+    if post:
+        if post.user_id == token_auth.current_user().id:
+            data = request.json
+    
+            title = data['title']
+            img_url = data['img_url']
+            caption = data['caption']
+                
+                    
+                    
+            post.title = title
+            post.img_url = img_url
+            post.caption = caption
+
+            post.saveChangesToDB()
+            return {
+                'status': 'ok',
+                'message': 'Succesfully updated your post!',
+                'post': post.to_dict()
+            }
+                   
+            
+        else:
+            return {
+                'status': 'not ok',
+                'message': "You cannot update another user's post."
+            }
+
+    else:
+        return {
+                'status': 'not ok',
+                'message': 'The post you are trying to update does not exist'
+            }
